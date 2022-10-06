e03bd29f668a49c720437a93764279766ac33178
ing11 <63070174@it.kmitl.ac.th>
2022-10-06 23:09:41 +0700

    ✨Add navLinks

---
+++ Theatre/src/constants/index.js
@@ -0,0 +1,30 @@
+export const navLinks = [
+    {
+        id: "history",
+        title: "History",
+    },
+    {
+        id: "turning_point",
+        title: "Turning Point",
+    },
+    {
+        id: "next_level",
+        title: "Next Level",
+    },
+    {
+        id: "stuff",
+        title: "Stuff",
+    },
+    {
+        id: "architecture",
+        title: "Architecture",
+    },
+    {
+        id: "publish",
+        title: "Publish",
+    },
+    {
+        id: "conclusion",
+        title: "Conclusion"
+    },
+];
