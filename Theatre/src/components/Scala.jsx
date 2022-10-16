693d6cc00df6748a6a27470900251d44ec33ea78
pawpawmasterpiece <ccoldplayy07@gmail.com>
2022-10-16 21:43:15 +0700

    1st-try
    Pilot F

--- Theatre/src/components/Scala.jsx
+++ Theatre/src/components/Scala.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './Scala.css';
+import ScalaLogo from '../img/Scala/Scala Logo.png'
 
 const Scala = () => {
   return (
-    <div>Scala</div>
+    <div>
+      <div>
+        <img src={ScalaLogo} alt="pic" width="100%" />
+          <div>
+            <p>ตำนาน “โรงภาพยนตร์สกาลา” ราชาโรงหนังแห่งสยามที่เป็นมากกว่าสถานที่ฉายหนัง <br />
+            แต่ยังดึงดูดกคนที่รักในการเสพความงามของสถาปัตยกรรม</p>
+          </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
