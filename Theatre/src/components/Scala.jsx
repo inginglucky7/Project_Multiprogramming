be48e190a1c62ab7b025d84371c6218b9ae0ab65
ing11 <63070174@it.kmitl.ac.th>
2022-10-18 02:52:23 +0700

    Revert

--- Theatre/src/components/Scala.jsx
+++ Theatre/src/components/Scala.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import './Scala.css';
 import ScalaLogo from '../img/Scala/Scala Logo.png'
-import Aura from '../img/Scala/Aura.png'
-import Car from '../img/Scala/Car.png'
 
 const Scala = () => {
   return (
     <div>
-      <div className='head1'>
-        <img className='scalalogo' src={ScalaLogo} />
+      <div>
+        <img src={ScalaLogo} alt="pic" width="100%" />
+          <div>
+            <p>ตำนาน “โรงภาพยนตร์สกาลา” ราชาโรงหนังแห่งสยามที่เป็นมากกว่าสถานที่ฉายหนัง <br />
+            แต่ยังดึงดูดกคนที่รักในการเสพความงามของสถาปัตยกรรม</p>
+          </div>
       </div>
-      <div className='head2'>
-          <p className='herotext'>ตำนาน “โรงภาพยนตร์สกาลา” ราชาโรงหนังแห่งสยามที่เป็นมากกว่าสถานที่ฉายหนัง <br />
-          แต่ยังดึงดูดกคนที่รักในการเสพความงามของสถาปัตยกรรม</p>
-      </div>
-      <div className='content1'>
-        <div className='divaura1'>
-          <img className='aura1' src={Aura} />
-        </div>
-        <div className='divaura2'>
-          <img className='aura2' src={Aura} />
-        </div>
-        <div className='divcar'>
-          <img className='car' src={Car} />
-        </div>
+      <div>
+        
       </div>
     </div>
   )
