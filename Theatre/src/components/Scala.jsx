114d4ed7344f580c58920e1e8f6924549cad8864
pawpawmasterpiece <ccoldplayy07@gmail.com>
2022-10-31 19:31:15 +0700

    Scala Pre-Animation
    mad with js

--- Theatre/src/components/Scala.jsx
+++ Theatre/src/components/Scala.jsx
@@ -10,21 +10,22 @@ import ScalaInside from '../img/Scala/Scala Inside.png'
 import ScalaPopcornCeiling from '../img/Scala/Scala Popcorn Ceiling.png'
 import ScalaMenWPack from '../img/Scala/Scala Men with Pack.png'
 import ScalaEx from '../img/Scala/Scala Ex.png'
+import * as ScalaJS from '../../scala';
 
 const Scala = () => {
   return (
     <div className='body'> 
       <div className='flex justify-center xs:mx-16 sm:mx-8'>
-        <img src={ScalaLogo} alt="logo" />
+        <img className='logo' src={ScalaLogo} alt="logo" />
       </div>
       <div className='flex justify-center text-center xs:mx-20 mt-32 mb-44 sm:my-20'>
-        <p className='text-white xs:text-base sm:text-xl'>ตำนาน “โรงภาพยนตร์สกาลา” ราชาโรงหนังแห่งสยามที่เป็นมากกว่าสถานที่ฉายหนัง <br />
+        <p id='txt-01' className='text-white xs:text-base sm:text-xl'>ตำนาน “โรงภาพยนตร์สกาลา” ราชาโรงหนังแห่งสยามที่เป็นมากกว่าสถานที่ฉายหนัง <br />
           แต่ยังดึงดูดกคนที่รักในการเสพความงามของสถาปัตยกรรม</p>
       </div>
-      <div className='xs:block md:hidden'><p className='text-white text-sm text-center mb-20'>• สกาลาเปิดฉายครั้งแรกเมื่อ <br />
+      <div className='xs:block md:hidden'><p id='txt-01' className='text-white text-sm text-center mb-20'>• สกาลาเปิดฉายครั้งแรกเมื่อ <br />
           วันที่ 31 ธันวาคม 2512</p></div>
       <div className='relative xs:mt-4 sm:mt-64'>
-        <div className='xs:hidden lg:block'><p className='text-white text-xl absolute left-1/2 top-28'>• สกาลาเปิดฉายครั้งแรกเมื่อ วันที่ 31 ธันวาคม 2512</p></div>
+        <div id='txt-01' className='xs:hidden lg:block'><p className='text-white text-xl absolute left-1/2 top-28'>• สกาลาเปิดฉายครั้งแรกเมื่อ วันที่ 31 ธันวาคม 2512</p></div>
         <div><img className='sm:scale-100 absolute xs:scale-150' src={ScalaAura} alt="aura" /></div>
         <div><img className='sm:scale-100 absolute xs:scale-150' src={ScalaContent} alt="content" /></div>
         <div><img className='sm:scale-100 absolute xs:scale-150' src={ScalaCar} alt="car" /></div>
@@ -36,11 +37,11 @@ const Scala = () => {
         <div className='sm:box row-start-1 xs:box'>
           <div className='flex'>
             <div className='sm:block xs:hidden'>
-              <img className='sm:ml-60 mb-4' src={ScalaJira} alt="jira" />
-              <p className='text-white sm:text-xl ml-64'>พันเอกจิระ ศิลป์กนก</p>
+              <img id='txt-01' className='sm:ml-60 mb-4' src={ScalaJira} alt="jira" />
+              <p id='txt-01' className='text-white sm:text-xl ml-64'>พันเอกจิระ ศิลป์กนก</p>
             </div>
             <div className='lg:block xs:hidden'>
-              <p className='text-white text-base ml-20 mt-12'>โรงหนังสกาลาถูกออกแบบโดยพันเอกจิระ ศิลป์กนก <br />
+              <p id='txt-01' className='text-white text-base ml-20 mt-12'>โรงหนังสกาลาถูกออกแบบโดยพันเอกจิระ ศิลป์กนก <br />
                 สถาปนิกที่มีชื่อเสียงโดดเด่นในช่วงหลังสงครามโลกครั้งที่ 2 <br />
                 ด้วยการใช้สถาปัตย์การออกแบบในศิลปะ Art Deco <br />
                 และได้ตั้งชื่อโรงภาพยนตร์ตั้งชื่อตามโรงอุปรากร Teatro alla Scala <br />
@@ -50,10 +51,10 @@ const Scala = () => {
             </div>
           </div>
           <div>
-            <div className='xs:block sm:hidden'>
-              <img className='mx-auto mt-12' src={ScalaJira} alt="jira" />
-              <p className='text-white text-center text-xl mt-4'>พันเอกจิระ ศิลป์กนก</p>
-              <p className='text-white text-center text-sm mt-16 mx-8'>โรงหนังสกาลาถูกออกแบบโดยพันเอกจิระ ศิลป์กนก <br />
+            <div id='txt-01' className='xs:block sm:hidden'>
+              <img id='txt-01' className='mx-auto mt-12' src={ScalaJira} alt="jira" />
+              <p id='txt-01' className='text-white text-center text-xl mt-4'>พันเอกจิระ ศิลป์กนก</p>
+              <p id='txt-01' className='text-white text-center text-sm mt-16 mx-8'>โรงหนังสกาลาถูกออกแบบโดยพันเอกจิระ ศิลป์กนก <br />
                 สถาปนิกที่มีชื่อเสียงโดดเด่นในช่วงหลังสงครามโลก <br />
                 ครั้งที่ 2 ด้วยการใช้สถาปัตย์การออกแบบในศิลปะ <br />
                 Art Deco และได้ตั้งชื่อโรงภาพยนตร์ตั้งชื่อตาม <br /> 
@@ -68,24 +69,24 @@ const Scala = () => {
         <div className='sm:box row-start-2 xs:box'>
           <div className='flex'>
             <div className='sm:block xs:hidden'>
-              <img className='ml-60' src={ScalaInside} alt="inside" />
+              <img id='txt-01' className='ml-60' src={ScalaInside} alt="inside" />
             </div>
             <div className='lg:block xs:hidden'>
-              <p className='text-white text-xl ml-52 mt-20'>• โคมไฟแก้วที่หรูหราทำให้อยากเดินเข้าไปค้นหาความงามในชั้นถัดไป</p>
+              <p id='txt-01' className='text-white text-xl ml-52 mt-20'>• โคมไฟแก้วที่หรูหราทำให้อยากเดินเข้าไปค้นหาความงามในชั้นถัดไป</p>
             </div>
           </div>
           <div>
             <div className='xs:block mt-24 sm:hidden'>
-              <img className='mx-auto w-4/6' src={ScalaInside} alt="inside" />
-              <p className='text-white text-xl text-center mx-8 mt-12'>• โคมไฟแก้วที่หรูหราทำให้อยากเดินเข้าไปค้นหาความงามในชั้นถัดไป</p>
+              <img id='txt-01' className='mx-auto w-4/6' src={ScalaInside} alt="inside" />
+              <p id='txt-01' className='text-white text-xl text-center mx-8 mt-12'>• โคมไฟแก้วที่หรูหราทำให้อยากเดินเข้าไปค้นหาความงามในชั้นถัดไป</p>
             </div>
           </div>
         </div>
 
         <div className='sm:box row-start-3 xs:box'>
           <div className='lg:block xs:hidden'>
-            <p className='text-white text-xl ml-60 mt-36'>สถาปัตยกรรม</p>
-            <p className='text-white text-base ml-60 mt-8'>โรงภาพยนตร์สกาลาเป็นงานศิลป์ขนาดใหญ่ผสมผสานตะวันออกและตะวันตกได้อย่างลงตัว <br />
+            <p id='txt-01' className='text-white text-xl ml-60 mt-36'>สถาปัตยกรรม</p>
+            <p id='txt-01' className='text-white text-base ml-60 mt-8'>โรงภาพยนตร์สกาลาเป็นงานศิลป์ขนาดใหญ่ผสมผสานตะวันออกและตะวันตกได้อย่างลงตัว <br />
               การตกแต่งมีทั้งไม้แกะสลักและภาพสำริดนูนต่ำ มีโถงบันไดขนาดใหญ่อันเป็นที่มาของชื่อ “สกาลา” <br />
               และโคมไฟแชนเดอเลียก็สั่งพิเศษมาจากอิตาลีเช่นกัน ตัวบันไดเป็นทางขึ้นสองทางเรียก Double Staircase <br />
               ทำจากคอนกรีต ปิดผิวขั้นบันไดด้วยหินอ่อน ตกแต่งราวจับด้วยอลูมิเนียมและไม้ <br />
@@ -105,8 +106,8 @@ const Scala = () => {
             </div>
           </div>
           <div className='xs:block sm:hidden mt-32'>
-            <p className='text-white text-xl text-center'>สถาปัตยกรรม</p>
-            <p className='text-white text-sm text-center mx-8 mt-8'>โรงภาพยนตร์สกาลาเป็นงานศิลป์ขนาดใหญ่ผสมผสาน <br />
+            <p id='txt-01' className='text-white text-xl text-center'>สถาปัตยกรรม</p>
+            <p id='txt-01' className='text-white text-sm text-center mx-8 mt-8'>โรงภาพยนตร์สกาลาเป็นงานศิลป์ขนาดใหญ่ผสมผสาน <br />
               ตะวันออกและตะวันตกได้อย่างลงตัว การตกแต่งมีทั้ง <br />
               ไม้แกะสลักและภาพสำริดนูนต่ำ มีโถงบันไดขนาดใหญ่ <br />
               อันเป็นที่มาของชื่อ “สกาลา” และโคมไฟแชนเดอเลีย <br />
@@ -123,24 +124,24 @@ const Scala = () => {
       <div className='sm:grid sm:grid-cols-2 sm:grid-rows-1 gap-2 xs:grid xs:grid-cols-1 xs:grid-rows-2'>
 
         <div className='sm:box xs:box'>
-          <div className='sm:block xs:hidden'>
+          <div id='txt-01' className='sm:block xs:hidden'>
             <img className='mx-auto' src={ScalaMenWPack} alt="menwithpack" />
           </div>
           <div className='xs:block sm:hidden'>
-            <img className='mx-auto w-5/6' src={ScalaMenWPack} alt="menwithpack" />
+            <img id='txt-01' className='mx-auto w-5/6' src={ScalaMenWPack} alt="menwithpack" />
           </div>
         </div>
 
         <div className='sm:box xs:box'>
           <div className='lg:block mt-32 xs:hidden'>
-            <p className='text-white text-xl'>จุดเปลี่ยน</p>
-            <p className='text-white text-base mt-8'>จนกระทั่งเมื่อวันที่ 5 กรกฎาคม พ.ศ. 2563 ทางโรงหนังได้ประกาศปิดตัว <br />
+            <p id='txt-01' className='text-white text-xl'>จุดเปลี่ยน</p>
+            <p id='txt-01' className='text-white text-base mt-8'>จนกระทั่งเมื่อวันที่ 5 กรกฎาคม พ.ศ. 2563 ทางโรงหนังได้ประกาศปิดตัว <br />
               เนื่องจากสาเหตุการแพร่ระบาดของเชื้อไวรัสโควิด-19 ในประเทศส่งผลทำให้ยอดผู้เข้าชมลดลง <br />
               ผนวกกับการแบกรับ ภาระต้นทุนการเช่าพื้นที่ไม่ไหว แม้สัญญาดังกล่าวจะสิ้นสุดในปี พ.ศ. 2564 </p>
           </div>
           <div className='xs:block mt-8 sm:hidden'>
-            <p className='text-white text-xl text-center'>จุดเปลี่ยน</p>
-            <p className='text-white text-sm text-center mx-8 mt-8'>จนกระทั่งเมื่อวันที่ 5 กรกฎาคม พ.ศ. 2563 <br />
+            <p id='txt-01' className='text-white text-xl text-center'>จุดเปลี่ยน</p>
+            <p id='txt-01' className='text-white text-sm text-center mx-8 mt-8'>จนกระทั่งเมื่อวันที่ 5 กรกฎาคม พ.ศ. 2563 <br />
               ทางโรงหนังได้ประกาศปิดตัว เนื่องจากสาเหตุ <br />
               การแพร่ระบาดของเชื้อไวรัสโควิด-19 ในประเทศ <br />
               ส่งผลทำให้ยอดผู้เข้าชมลดลง ผนวกกับการแบกรับภาระ <br />
@@ -152,7 +153,7 @@ const Scala = () => {
 
       <div className='mt-64'>
         <div className='lg:block xs:hidden'>
-          <p className='mx-auto text-center text-white text-lg'>สกาลาจึงปิดตำนานโรงหนังสุดคลาสสิคของชาวสยามสแควร์ไปอย่างเป็นทางการ <br />
+          <p id='txt-01' className='mx-auto text-center text-white text-lg'>สกาลาจึงปิดตำนานโรงหนังสุดคลาสสิคของชาวสยามสแควร์ไปอย่างเป็นทางการ <br />
             เมื่อวันที่ 1 พฤศจิกายน พ.ศ. 2563</p>
         </div>
         <div className='sm:block xs:hidden relative'>
@@ -162,7 +163,7 @@ const Scala = () => {
 
       <div className='-mt-28'>
         <div className='xs:block sm:hidden'>
-          <p className='mx-12 mb-16 text-center text-white text-base'>สกาลาจึงปิดตำนานโรงหนังสุดคลาสสิค <br /> 
+          <p id='txt-01' className='mx-12 mb-16 text-center text-white text-base'>สกาลาจึงปิดตำนานโรงหนังสุดคลาสสิค <br /> 
             ของชาวสยามสแควร์ไปอย่างเป็นทางการ <br />
             เมื่อวันที่ 1 พฤศจิกายน พ.ศ. 2563</p>
         </div>
