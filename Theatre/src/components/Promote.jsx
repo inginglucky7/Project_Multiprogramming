d73069584d869ab580a613600170525b55b074da
pawpawmasterpiece <ccoldplayy07@gmail.com>
2022-11-21 18:35:14 +0700

    Promote-Car
    Yeah

--- Theatre/src/components/Promote.jsx
+++ Theatre/src/components/Promote.jsx
@@ -11,16 +11,29 @@ import PMRedCar from '../img/Promote/PMRedCar.png'
 import PMWatch from '../img/Promote/PMWatch.png'
 import PMWallet from '../img/Promote/PMWallet.png'
 import PMText2 from '../img/Promote/PMTXT2.png'
+<<<<<<< Updated upstream
 import * as ScalaJS from '../js/scala.js';
+=======
+import PMMobileBG from '../img/Promote/PMMobileBG.png'
+import PMMobilePaper from '../img/Promote/PMMobilePaper.png'
+import PMMobileTXT from '../img/Promote/PMMobileTXT.png'
+import * as ScalaJS from '../../scala';
+>>>>>>> Stashed changes
 
 const Promote = () => {
   return (
     <div className='promotebody'>
 
-      <div>
+      <div className='xs:hidden sm:block'>
         <img className='absolute scale-100' src={PMRollPaper} alt="pmpaper" />
         <img id='txt-01' className='absolute scale-100' src={PMText} alt="pmtxt" />
         <img className='scale-100' src={PMRollPaperBG} alt="pmpaperbg" />
+      </div>
+
+      <div className='sm:hidden xs:block'>
+        <img className='absolute scale-100' src={PMMobilePaper} alt="pmpaper" />
+        <img id='txt-01' className='absolute scale-100' src={PMMobileTXT} alt="pmtxt" />
+        <img className='scale-100' src={PMMobileBG} alt="pmpaperbg" />
       </div>
 
       <div className='xs:hidden sm:block'>
